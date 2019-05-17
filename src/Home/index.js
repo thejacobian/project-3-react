b089def1515f57322543231e997dc8a262d88524
iamconnaught <wdonnelly22@gmail.com>
2019-05-16 21:28:47 -0500

    updated homepage with new logo

--- src/Home/index.js
+++ src/Home/index.js
@@ -3,10 +3,10 @@ import { Link }from 'react-router-dom';
 
 class Home extends Component{
 	render(){
-		// const logo = require('../public/Artboard 1@3x.png')
+		const logo = require('../public/BUCKETLIST HHORIZONTAL.png')
 		return(
 			<div className="home">
-				<h1>Music Bucketlist</h1>
+				<img src={logo} />
 				<p>Log in to add concerts you have attended to your profile and start your Bucketlist of artists you hope to see</p>
 				<span><Link to='/auth/login'>Login</Link></span>
 				<span><Link to='/auth/register'>Register</Link></span>
